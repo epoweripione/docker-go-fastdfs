a217b2806003aacb1a3e6458fdad3485884c6b81
siyuwuxin <siyuwuxin@gmail.com>
2019-07-08 16:18:34 +0800

    move go-fastdfs-web runtime dir

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,7 +2,13 @@
 
 # ps -ef |grep java|grep go-fastdfs-web.jar|grep -v grep
 
-cd /opt/go-fastdfs-web/
+if [ -s "/go-fastdfs-web/go-fastdfs-web.jar" ]; then
+    cp -f /opt/go-fastdfs-web/go-fastdfs-web.jar /go-fastdfs-web/go-fastdfs-web.jar
+else
+    cp -fr /opt/go-fastdfs-web /
+fi
+
+cd /go-fastdfs-web
 nohup java -jar go-fastdfs-web.jar > /dev/null 2>&1 &
 
 cd $INSTALL_DIR
